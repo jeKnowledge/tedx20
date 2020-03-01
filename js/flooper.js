8957c6ee06886f4b033a060408757d55ff5e15d0
dbgfribeiro <dribeiro641@gmail.com>
2020-03-01 18:26:28 +0000

    First commit

---
+++ js/flooper.js
@@ -0,0 +1,32 @@
+let myFlooper2 = new flooper('.js-flooper2', {
+  autoPlay: true,
+  bufferSize: 10,
+  speed: 1,
+  onFloop: () => {},
+  onStart: () => {},
+  onPlay: () => {},
+  onPause: () => {},
+  onSlowmotion: () => {},
+});
+
+let myFlooper3 = new flooper('.js-flooper3', {
+  autoPlay: true,
+  bufferSize: 10,
+  speed: 1,
+  onFloop: () => {},
+  onStart: () => {},
+  onPlay: () => {},
+  onPause: () => {},
+  onSlowmotion: () => {},
+});
+
+let myFlooper4 = new flooper('.js-flooper4', {
+  autoPlay: true,
+  bufferSize: 10,
+  speed: 1,
+  onFloop: () => {},
+  onStart: () => {},
+  onPlay: () => {},
+  onPause: () => {},
+  onSlowmotion: () => {},
+});
