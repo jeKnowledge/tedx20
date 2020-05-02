8fd353d3c52e55d625b55b44caf457b1eab5f8aa
dbgfribeiro <dribeiro641@gmail.com>
2020-05-02 02:51:01 +0100

    final changes

--- js/orators.js
+++ js/orators.js
@@ -13,23 +13,14 @@ var n = document.getElementById("name");
 var d = document.getElementById("description");
 
 var claraNao = document.getElementById("clara-nao");
-var arlindoOliveira = document.getElementById("arlindo-oliveira");
 var anaCosta = document.getElementById("ana-costa");
 var carolinaPereira = document.getElementById("carolina-pereira");
-var zeMagico = document.getElementById("ze-magico");
 
 /*Clara Não*/
 if (claraNao.addEventListener) {
   claraNao.addEventListener("click", function() {
     n.innerText = "Clara Não";
     d.innerText = "Clara Não nasceu e vive no Porto. Licenciou-se em Design de Comunicação pela Faculdade de Belas Artes no Porto e foi devido à sua experiência de erasmus em Roterdão que direcionou o seu caminho pela ilustração e pela escrita. Há 2 anos voltou à Faculdade de Belas Artes onde tirou o seu mestrado em desenho e técnicas de impressão. Clara Não destaca-se pela sua ironia e irreverência, nas ilustrações onde reivindica a igualdade, trata de tabus sociais e desenvolve a escrita ambidestra. Está representada em diversas galerias, como a Senhora Presidenta, Circus Network, Ó! Galeria, Malapata, Apaixonarte e Área 55. É mencionada de forma regular em diversas publicações, como a Vogue, Público e Jornal de Notícias, e tem uma forte presença diariamente no Instagram.";
-  });
-}
-/*Arlindo Oliveira*/
-if (arlindoOliveira.addEventListener) {
-  arlindoOliveira.addEventListener("click", function() {
-    n.innerText = "Arlindo Oliveira";
-    d.innerText = "Arlindo Oliveira é um reconhecido investigador, académico e escritor português, na área das ciências de computação. Professor do departamento de Engenharia Eletrotécnica e de Computadores do IST (Lisboa), os seus principais interesses são: Algoritmos e Complexidade, Bioinformática e Design de Circuitos Digitais. O seu livro mais recente é Inteligência Artificial publico pela Fundação Francisco Manuel dos Santos. Doutorado na Universidade de Califórnia, Berkeley, é membro da Academia Portuguesa de Engenharia e membro sénior da IEEE.";
   });
 }
 /*Ana Filpa Costa*/
@@ -46,10 +37,24 @@ if (carolinaPereira.addEventListener) {
     d.innerText = "Com apenas 19 anos Carolina Salgueiro Pereira, além de estudante de educação física é também fundadora do My Destiny, um projeto internacional de voluntariado através do surf. É ainda criadora da marca criadoras da marca Frozen, que inclui uma linha de roupa, um canal online e pranchas de surf. Carolina faz também parte da HeForShe Portugal e é voluntária na organização SOS Salvem o Surf.";
   });
 }
-/*Zé Mágico*/
-if (zeMagico.addEventListener) {
-  zeMagico.addEventListener("click", function() {
-    n.innerText = "Zé Mágico";
-    d.innerText = "Nascido a 1994 e conhecido como o “Zé Mágico”, este conheceu a sua paixão pelo ilusionismo com apenas 15 anos. Através de livros, vídeos, espetáculos e todos os formatos a que tinha acesso, conseguiu tornar-se num dos maiores ilusionistas do nosso país. Aparte da sua formação em Design de Mobiliário, também se apaixonou pelo Teatro Experimental, o que considerou fundamental na construção da sua personalidade e de todas as personagens que daí surgiram. Com os seus diverso espetáculos e eventos personalizados, garante criar um ambiente mágico nas suas atuações com o seu carismo e atitude única.";
-  });
-}
+$(document).ready(function() {
+  if ($(window).width() <= 800) {
+    $('#clara-nao-img').attr('src', 'img/orators/Clara Nao 1.jpg');
+    $('#clara-nao-img').attr('onmouseover', false);
+    $('#clara-nao-img').attr('onmouseout', false);
+  }
+});
+$(document).ready(function() {
+  if ($(window).width() <= 800) {
+    $('#ana-costa-img').attr('src', 'img/orators/Ana Costa 1.jpg');
+    $('#ana-costa-img').attr('onmouseover', false);
+    $('#ana-costa-img').attr('onmouseout', false);
+  }
+});
+$(document).ready(function() {
+  if ($(window).width() <= 800) {
+    $('#carolina-pereira-img').attr('src', 'img/orators/Carolina Salgueiro Pereira.jpg');
+    $('#carolina-pereira-img').attr('onmouseover', false);
+    $('#carolina-pereira-img').attr('onmouseout', false);
+  }
+});
