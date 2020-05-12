8a00c56c7ceb5a9eca742439d7771788f8cd130d
dbgfribeiro <dribeiro641@gmail.com>
2020-05-12 18:50:54 +0100

    Updated orators & fixed meta tags

--- js/orators.js
+++ js/orators.js
@@ -15,12 +15,15 @@ var d = document.getElementById("description");
 var claraNao = document.getElementById("clara-nao");
 var anaCosta = document.getElementById("ana-costa");
 var carolinaPereira = document.getElementById("carolina-pereira");
+var eduardoJorge = document.getElementById("eduardo-jorge");
+var fatimaCastro = document.getElementById("fatima-castro");
+var goulds = document.getElementById("goulds");
 
 /*Clara Não*/
 if (claraNao.addEventListener) {
   claraNao.addEventListener("click", function() {
     n.innerText = "Clara Não";
-    d.innerText = "Clara Não nasceu e vive no Porto. Licenciou-se em Design de Comunicação pela Faculdade de Belas Artes no Porto e foi devido à sua experiência de erasmus em Roterdão que direcionou o seu caminho pela ilustração e pela escrita. Há 2 anos voltou à Faculdade de Belas Artes onde tirou o seu mestrado em desenho e técnicas de impressão. Clara Não destaca-se pela sua ironia e irreverência, nas ilustrações onde reivindica a igualdade, trata de tabus sociais e desenvolve a escrita ambidestra. Está representada em diversas galerias, como a Senhora Presidenta, Circus Network, Ó! Galeria, Malapata, Apaixonarte e Área 55. É mencionada de forma regular em diversas publicações, como a Vogue, Público e Jornal de Notícias, e tem uma forte presença diariamente no Instagram.";
+    d.innerText = "Para nos presentear com a Metamorfose de Mentalidades, contaremos com a presença de Clara Não, nome artístico de Clara Silva. Nasceu em Cedofeita, Porto e foi criada em Grijó, Aveiro. Licenciou-se em Design de Comunicação, na Faculdade de Belas Artes da Universidade do Porto e, tornou-se mestre em Desenho e Técnicas da Impressão na mesma instituição, após ter feito Erasmus em Roterdão. A autora do livro Miga, esquece lá isso!, lançado em Junho 2019, defende que o nosso superpoder é dizer “Não” e o seu trabalho capta a atenção por falar sem tabu de aflições do quotidiano de cada um. Começou por publicar apenas conteúdo na rede social Instagram, mas atualmente o seu trabalho é exposto frequentemente em jornais de referência e em várias galerias. Fazer tapeçaria manual, ser ambidestra e DJ fazem são apenas algumas das suas inúmeras aptidões.";
   });
 }
 /*Ana Filpa Costa*/
@@ -34,9 +37,33 @@ if (anaCosta.addEventListener) {
 if (carolinaPereira.addEventListener) {
   carolinaPereira.addEventListener("click", function() {
     n.innerText = "Carolina Salgueiro Pereira";
-    d.innerText = "Com apenas 19 anos Carolina Salgueiro Pereira, além de estudante de educação física é também fundadora do My Destiny, um projeto internacional de voluntariado através do surf. É ainda criadora da marca criadoras da marca Frozen, que inclui uma linha de roupa, um canal online e pranchas de surf. Carolina faz também parte da HeForShe Portugal e é voluntária na organização SOS Salvem o Surf.";
+    d.innerText = "Inserido no mote “Metamorfose Humanitária”, o qual remete para a impreteribilidade de nos edificarmos constantemente no sentido lato, procurando guindar o nosso potencial em causas humanísticas que fomentem o respeito por todos os seres humanos e reconhecendo que “todo o mundo é composto de mudança, tomando sempre novas qualidades”, a presente edição do TEDxUniversidadedeCoimbra contará com a partilha de experiências ricas de Carolina Salgueiro Pereira, de vinte e sete anos. Ativista em direitos humanos e storyteller da ONU, preconiza, nas suas palavras, que “a vida é demasiado curta para ser pequena”, imperando, por isso, pugnarmos por tudo aquilo em que acreditamos, sem nunca nos reduzirmos enquanto pessoas. Apresenta um vasto leque profissional, sendo que, com dezanove anos, fundou a My Destiny, um projeto educacional internacional inerente ao voluntariado a partir do surf. Nesse prisma, é voluntária na organização SOS Salvem o Surf, tendo igualmente criado uma marca que inclui uma linha de roupa, um canal online e pranchas de surf. Anos mais tarde, abraçou causas sociais e humanitárias, fazendo parte intrínseca da HeForShe, que defende ser da máxima relevância capacitar as mulheres no sentido de pelejar por uma sociedade mais justa nos valores de promoção da igualdade de género. Ainda importante, é ativista na UN SDG Action Campaign e Codiretora da Sathyam Project, cuja missão passa por promover a metamorfose da vida de meninas e mulheres da Índia, investindo na educação e acesso a oportunidades a fim de quebrar ciclos de pobreza nas suas comunidades, criando, para tal, empreendimentos sociais sustentáveis que forneçam oportunidades de trabalho, desprovidos de violência.";
   });
 }
+/*Eduardo Jorge*/
+if (eduardoJorge.addEventListener) {
+  eduardoJorge.addEventListener("click", function() {
+    n.innerText = "Eduardo Jorge";
+    d.innerText = "Eduardo Jorge foi obrigado a adaptar-se a uma nova realidade quando, em 1991, ficou tetraplégico após um acidente de viação. Testemunho real de uma “Metamorfose na Sociedade”, é pela independência e igualdade que ainda hoje luta. Em 2006, criou o blog “Tetraplégicos” com o intuito de partilhar as suas experiências e, em 2018, protestou em frente da Assembleia da República. Após uma viagem de 3 dias de cadeira de rodas entre a sua casa em Concavada, Abrantes e Lisboa, fez greve de fome e dormiu numa cama com grades, imitando uma jaula. O seu objetivo foi demonstrar a falta de liberdade e o quão negligenciada é a comunidade tetraplégica em Portugal. O projeto de lei “Modelo de Apoio à Vida Independente” estava há mais de um ano para ser aplicado e Eduardo Jorge queria ser ouvido pelo nosso governo. Apenas Marcelo compareceu no protesto e, juntos discutiram o projeto que tem como objetivo permitir que tetraplégicos possam ter maior autonomia ao viver em suas casas, com o apoio de um cuidador, em vez de em lares ou outras instituições. Após o próprio Eduardo Jorge ter vivido 4 anos num lar, no qual trabalhava, regressou finalmente à sua casa em Julho de 2019, quase dois anos após a aprovação do projeto. Foi em sua casa que voltou a falar com Marcelo de Sousa, acompanhado também da Secretária da Inclusão, Ana Sofia Antunes.";
+  });
+}
+/*Fatima Castro*/
+if (fatimaCastro.addEventListener) {
+  fatimaCastro.addEventListener("click", function() {
+    n.innerText = "Fátima Velez de Castro";
+    d.innerText = "Fátima Velez de Castro será a oradora do TEDx que desvendará os segredos da “Metamorfose da Educação”. É uma investigadora e professora da Universidade de Coimbra, natural de Arronches, Portalegre. Formada em Geografia e amante da escrita desde jovem, em 2012 lançou o livro “O eterno e o efémero”. Conta com a publicação de vários contos, tendo o “Frôntera” sido motivo da sua nomeação e atribuição de vários prémios literários. Juntamente com o seu irmão, atualmente publica crónicas no jornal regional de Arronches. Realizou mestrado em estudos europeus e está atualmente a terminar o pré doutoramento em Literatura. É ainda coordenadora do Conselho de Formação de Professores da FLUC e do Mestrado em Ensino da Geografia no 3º ciclo e Ensino Secundário, na mesma instituição.";
+  });
+}
+/*Goulds*/
+if (goulds.addEventListener) {
+  goulds.addEventListener("click", function() {
+    n.innerText = "Goulds";
+    d.innerText = "Em 2016, nasce Goulds, uma personagem que representa a força, carisma e sensualidade da mulher. Para Daniel Araújo, designer de 21 anos, viver na pele de uma “drag queen” começou como apenas uma curiosidade. Porém, rapidamente se apercebeu do poder e ausência de limitações que esta personificação acarreta. Ser Glouds permite-lhe explorar o mundo da fantasia e tecer críticas aos mais variados temas geradores de controvérsia na nossa sociedade. Num registo inesperado de individualidade e liberdade de expressão, Goulds é o convidado certo para nos falar da importância da “Metamorfose Pessoal” rumo à aceitação daquilo que somos, sentimos e pensamos.";
+  });
+}
+
+
+
 $(document).ready(function() {
   if ($(window).width() <= 800) {
     $('#clara-nao-img').attr('src', 'img/orators/Clara Nao 1.jpg');
@@ -48,5 +75,15 @@ $(document).ready(function() {
     $('#carolina-pereira-img').attr('src', 'img/orators/Carolina Salgueiro Pereira.jpg');
     $('#carolina-pereira-img').attr('onmouseover', false);
     $('#carolina-pereira-img').attr('onmouseout', false);
+    $('#eduardo-jorge-img').attr('src', 'img/orators/Eduardo1.jpg');
+    $('#eduardo-jorge-img').attr('onmouseover', false);
+    $('#eduardo-jorge-img').attr('onmouseout', false);
+    $('#fatima-castro-img').attr('src', 'img/orators/Fatima1.jpg');
+    $('#fatima-castro-img').attr('onmouseover', false);
+    $('#fatima-castro-img').attr('onmouseout', false);
+
+    $('#goulds-img').attr('src', 'img/orators/Goulds1.jpg');
+    $('#goulds-img').attr('onmouseover', false);
+    $('#goulds-img').attr('onmouseout', false);
   }
 });
